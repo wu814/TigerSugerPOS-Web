17dc73a2977e070b3a55925e7e344b84d5e6145a
Christopher Vu <christophervu03@gmail.com>
2023-10-31 15:28:36 -0500

    added manager buttons and pages

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,11 +1,41 @@
 import styles from './page.module.css'
 import Navbar from '../../components/Navbar'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
         <Navbar/>
-        <p>Manager page (replace later)</p>
+        <div className={styles.main}>
+            <h1 className={styles.mainHeading}>Manager View</h1>
+
+            <div className={styles.horizontalContainer}>
+                <div className={`${styles.verticalContainer} ${styles.buttonContainer}`}>
+                    <Link role="link" className={styles.button} href="/manager/menu">
+                        Edit Menu
+                    </Link>
+                    <Link role="link" className={styles.button} href="/manager/inventory">
+                        View Inventory
+                    </Link>
+                </div>
+
+                <div className={`${styles.verticalContainer} ${styles.buttonContainer}`}>
+                    <Link role="link" className={styles.button} href="/manager/salesreport">
+                        Sales Report
+                    </Link>
+                    <Link role="link" className={styles.button} href="/manager/restockreport">
+                        Restock Report
+                    </Link>
+                    <Link role="link" className={styles.button} href="/manager/popularpairs">
+                        Popular Pairs
+                    </Link>
+                    <Link role="link" className={styles.button} href="/manager/usagechart">
+                        Usage Chart
+                    </Link>
+                </div>
+            </div>
+        </div>
+
     </>
   )
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,30 @@
 import { Pool, QueryResult } from 'pg';
 
-const pool = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password, // Replace with your actual password
-    port: 5432,
-});
+const createPool = () => {
+    console.log('Creating new pool');
 
+    return new Pool({
+        user: process.env.user,
+        host: process.env.host,
+        database: process.env.database,
+        password: process.env.password,
+        port: 5432,
+    });
+}
+
+let pool: Pool;
+
+const globalForPool = global as unknown as { pool: Pool };
+
+if (process.env.NODE_ENV === 'production') {
+    pool = createPool();
+} else {
+    if (!globalForPool.pool) {
+      globalForPool.pool = createPool();
+    }
+    pool = globalForPool.pool;
+}
+  
 export async function query(text: string, params?: any[]): Promise<QueryResult> {
     const start = Date.now();
     const client = await pool.connect();
