33a186d7263a508876dd913fe2036f3d15da9a8e
wu814 <william14.wu@gmail.com>
2023-10-31 19:17:43 -0500

    added placeOrder button on cashier, for now it only place an order with default values

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -1,11 +1,41 @@
+"use client"; // necessary for useState to work
+import { NextResponse } from "next/server";
 import styles from './page.module.css'
 import Navbar from '../../components/Navbar'
+import { useState } from 'react';
 
 export default function Home() {
+  const [apiResponse, setApiResponse] = useState(null);
+  const placeOrder = async () => {
+    const orderData = { // Define orderData as an object
+        order_timestamp: "2023-10-29 14:33:00",
+        employee_id: 1,
+        customer_id: 2,
+        order_items: ["item1", "item2"],
+        order_total: 25.00,
+        drink_attributes: ["item1", "item2"],
+        drink_addons: ["item1", "item2"],
+    };
+    
+    try{
+      const response = await fetch("/api/placeOrder", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+      });
+    }catch(error){
+      console.error('Error fetching API:', error);
+    }
+  }
+  
   return (
-    <>
+      <>
         <Navbar/>
         <p>Customer page (replace later)</p>
+        <button onClick={placeOrder}>Test API Endpoint</button>
+
     </>
   )
 }
