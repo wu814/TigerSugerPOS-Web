493b80246f911759b8146043bd6be3a6e26bb455
wu814 <william14.wu@gmail.com>
2023-11-14 20:27:38 -0600

    place order can now take actually parameters

--- src/app/api/placeOrder/route.ts
+++ src/app/api/placeOrder/route.ts
@@ -7,12 +7,12 @@ export async function POST(request: NextRequest) {
         const reqData = await request.json();
         console.log(reqData);
         // Extract order details from the request data
-        const { order_timestamp, employee_id, customer_id, order_items, order_total, drink_attributes, drink_addons } = reqData;
+        const { orderTimestamp, employeeId, customerId, orderItems, orderTotal, drinkAttributes, drinkAddOns } = reqData;
         // Construct the SQL query to insert a new order
         const insertQuery = "INSERT INTO orders (order_timestamp, employee_id, customer_id, order_items, order_total, drink_attributes, drink_addons) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING order_id";
 
         // Execute the query with parameters
-        const result = await query(insertQuery, [order_timestamp, employee_id, customer_id, order_items, order_total, drink_attributes, drink_addons]);
+        const result = await query(insertQuery, [orderTimestamp, employeeId, customerId, orderItems, orderTotal, drinkAttributes, drinkAddOns]);
 
         // Retrieve the newly generated order_id
         const { order_id } = result.rows[0];
