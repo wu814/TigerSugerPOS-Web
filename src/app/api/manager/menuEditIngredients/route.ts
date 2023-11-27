e5244c784440825c1fd74b9fbe5f23395f009ac9
JoshHare <jmhhare@gmail.com>
2023-11-27 17:25:05 -0600

    Edit Ingredients Endpoint

---
+++ src/app/api/manager/menuEditIngredients/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { query } from "../../../../utils/database";
+
+//edit the ingredients of a menu item
+export async function POST(request: NextRequest) {
+    try{
+        const data = await request.json();
+        const {drink,ing} = data;
+
+        //check if valid
+        const dupeQuery = "SELECT * FROM products WHERE drink_name = $1;";
+        const dupe = await query(dupeQuery,[drink]);
+        if(dupe.rows.length == 0){
+            return NextResponse.json({error:"Item does not exist on Menu!"},{status: 500});
+        }
+
+
+        const sql = "UPDATE products SET ingredients = $1 WHERE drink_name = $2";
+        const result = await query(sql,[ing,drink]);
+        return NextResponse.json({ message: "Type of "+drink+" changed successfully" }, { status: 200 });
+    }
+    catch(error:any){
+       console.log("ASDF");
+        return NextResponse.json({error: error.toString()},{status: 500});
+    }
+}
