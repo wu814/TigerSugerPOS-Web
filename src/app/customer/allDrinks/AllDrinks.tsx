2303b3954007c1ba4e018318ed0fadb9ffc1cd58
tlong36 <113958217+tlong36@users.noreply.github.com>
2023-11-29 13:36:12 -0600

    added all drinks option

---
+++ src/app/customer/allDrinks/AllDrinks.tsx
@@ -0,0 +1,50 @@
+"use client"; // necessary for useState to work
+import styles from './page.module.css'
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function AllDrinks({ addToCart }: { addToCart: any }) {
+   
+    const [menuData, setMenuData] = useState<any[]>([]); // for fetching menu data
+
+    const fetchMenu = async () => {
+        const response = await fetch('/api/manager/menuDisplay');
+        const json = await response.json();
+        setMenuData(json.message);
+    }
+
+    // Fetch menu data on page load
+    useEffect(() => {
+        fetchMenu();
+    },[]);
+
+    const handleOrderSelection = (menuItem: any) => {
+        addToCart(menuItem);
+    };
+
+    return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          {menuData
+            .map((menuItem, index) => (
+              <div className={styles.imageContainer} key={index}>
+                {/* Wrap the Image inside a Link so it's clickable */}
+                <Image
+                    src={`/images/${menuItem.image_url}`}
+                    alt={`Boba Drink ${index + 1}`}
+                    width={300}
+                    height={300}
+                    className={styles.image}
+                />
+                <p className={styles.drinkName}>{menuItem.drink_name}</p>
+                <p className={styles.drinkPrice}>${menuItem.price}</p>
+                <button onClick={() => handleOrderSelection(menuItem)}>Add to Order</button>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
