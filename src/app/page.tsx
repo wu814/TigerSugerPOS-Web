f6059fce96f283d8f38b72e60c305b9869ca2e75
Christopher Vu <christophervu03@gmail.com>
2023-12-06 13:23:28 -0600

    changing landing page to have 2 headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,12 @@ export default function Home() {
             
             <div className={styles.main}>
                 <div className = {styles.content}>
-                {session && <h1 className={styles.user}>Welcome, {session?.user?.name}!</h1>}
-                Welcome to Tiger Sugar! Click on one of the links above to get started.
+                {session ? (
+                    <h1 className={styles.user}>Welcome, {session?.user?.name}!</h1>
+                ) : ( 
+                    <h1 className={styles.user}>Welcome to Tiger Sugar!</h1>
+                )}
+                <h2>Click on one of the links above to get started.</h2>
                 <br></br>
                 </div>
             </div>
